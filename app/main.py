e43e197b95542ecd5dc13ec891f0c27d8324323f
Murilo Sena <132272353+0mss@users.noreply.github.com>
2025-06-07 01:12:48 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -22,5 +22,4 @@ app.include_router(auth_router.router)
 app.include_router(media_router.router)
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # pega a PORT ou usa 8000 padrão
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True)
